a1afa09312c6db007df32042a85e1a128819fc94
Ray Neumann <rayneumann.de@gmail.com>
2018-03-12 10:18:35 +0100

    adding files

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="NewQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDelted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a Quote in Order to Delete it.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuoteGrid from "./components/QuoteGrid.vue";
+    import NewQuote from "./components/NewQuote.vue";
+    import Header from "./components/Header.vue";
+    export default {
+        data: function() {
+            return {
+                quotes: [
+                    'Just a QUOTE to see SoMeThInG'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            NewQuote(quote) {
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header
+        }
+    }
+</script>
+
+<style>
+</style>
